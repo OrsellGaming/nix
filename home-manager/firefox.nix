1828e779ec3045bb8772c2b724c43d419dbe0563
BackSlashN <thewoodster333@gmail.com>
2025-06-08 15:07:02 -0400

    Begone google! (and other spyware settings)

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -1,9 +1,59 @@
 { inputs, pkgs, ... }:
 {
-  programs.firefox = {
+  programs.firefox = let
+    lock-false = {
+      Value = false;
+      Status = "locked";
+    };
+    lock-true = {
+      Value = true;
+      Status = "locked";
+    };
+  in {
     enable = true;
     nativeMessagingHosts = with pkgs; [ uget-integrator ];
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+
+      Preferences = {
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.system.topstories" = lock-false;
+        "browser.newtabpage.activity-stream.section.topstories.rows" = "0";
+        "services.sync.prefs.sync.browser.newtabpage.activity-stream.section.topstories.rows" = "0";
+        "services.sync.prefs.sync.browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+      };
+    };
+
     profiles.n = {
+      search = {
+        force = true;
+        default = "Brave";
+        engines = {
+          "Brave" = {
+            urls = [{template = "https://search.brave.com/search?q={searchTerms}";}];
+            definedAliases = ["@b"];
+          };
+        };
+      };
+
       extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
         ublock-origin
       ];
