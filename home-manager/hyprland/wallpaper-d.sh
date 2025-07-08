5f9e2efbe6a1fcf37e39b4572d53fc814a62337e
BackSlashN <thewoodster333@gmail.com>
2025-07-07 20:50:30 -0400

    manage videos in script instead of mpv playlist

--- home-manager/hyprland/wallpaper-d.sh
+++ home-manager/hyprland/wallpaper-d.sh
@@ -1,8 +1,9 @@
 # WALLPAPER MANAGER
 while :; do
-  if [[ $(date +%T) < 19:00:00 ]]; then # 7 pm
-    mpvpaper --mpv-options "--loop-file=300 --reset-on-next-file=loop-file" ALL ~/.wallpapers/playlist-day.m3u
+  if [[ $(date +%T) < 19:00:00 ]]; then # before 7 pm
+    mpvpaper --mpv-options "--loop-file=300" ALL ~/.wallpapers/spill-animated.mp4
+    mpvpaper --mpv-options "--loop-file=120" ALL ~/.wallpapers/ila-animated.mp4
   else
-    mpvpaper --mpv-options "--loop-file=300 --reset-on-next-file=loop-file" ALL ~/.wallpapers/playlist-night.m3u
+    mpvpaper --mpv-options "--loop-file=1000" ALL ~/.wallpapers/overthrower-animated.mp4
   fi
 done
