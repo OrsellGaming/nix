e521b86092d1e85d85967aada0d295e72005ea97
BackSlashN <thewoodster333@gmail.com>
2025-07-09 16:12:49 -0400

    add condition to have dark wallpapers after midnight

--- home-manager/hyprland/wallpaper-d.sh
+++ home-manager/hyprland/wallpaper-d.sh
@@ -1,9 +1,9 @@
 # WALLPAPER MANAGER
 while :; do
-  if [[ $(date +%T) < 19:00:00 ]]; then # before 7 pm
+  if [[ $(date +%T) < 19:00:00 ]] || [[ $(date +%T) > 6:00:00 ]]; then # before 7 PM or after 6 AM (daytime)
     mpvpaper --mpv-options "--loop-file=300" ALL ~/.wallpapers/spill-animated.mp4
     mpvpaper --mpv-options "--loop-file=120" ALL ~/.wallpapers/ila-animated.mp4
-  else
+  else # night time
     mpvpaper --mpv-options "--loop-file=250" ALL ~/.wallpapers/overthrower-animated.mp4
     mpvpaper --mpv-options "--loop-file=600" ALL ~/.wallpapers/team.mp4
   fi
