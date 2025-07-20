1af6fc6e22a0dd1348b50d42b4db21e1c7b5eb14
BackSlashN <thewoodster333@gmail.com>
2025-07-20 12:10:18 -0400

    fixed wallpaper logic (for real this time)

--- home-manager/hyprland/wallpaper-d.sh
+++ home-manager/hyprland/wallpaper-d.sh
@@ -1,6 +1,6 @@
 # WALLPAPER MANAGER
 while :; do
-  if [[ $(date +%T) < 19:00:00 ]] && [[ $(date +%T) > 6:00:00 ]]; then # before 7 PM and after 6 AM (daytime)
+  if [[ $(date +%T) < 19:00:00 ]] || [[ $(date +%T) > 6:00:00 ]] && [[ $(date +%T) > 0:59:59 ]]; then # before 7 PM and after 6 AM (daytime)
     mpvpaper --mpv-options "--loop-file=300" ALL ~/.wallpapers/spill-animated.mp4
     mpvpaper --mpv-options "--loop-file=120" ALL ~/.wallpapers/ila-animated.mp4
   elif [[ $(date +%T) > 19:00:00 ]] && [[ $(date +%T) < 23:00:00 ]]; then # after 7 PM and before 11pm
