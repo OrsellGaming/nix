4ec6f534603ee8cec5341393df33ac474c2d11f4
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2025-10-15 11:22:49 -0700

    fix: rofi-wayland has been merged into rofi

--- home-manager/rofi/rofi.nix
+++ home-manager/rofi/rofi.nix
@@ -9,7 +9,7 @@
         enable = true;
         location = "bottom";
         theme = "~/.config/rofi/themes/catppuccin-macchiato.rasi";
-        plugins = with pkgs; [ rofi-calc rofi-emoji-wayland rofi-power-menu ];
+        plugins = with pkgs; [ rofi-calc rofi-emoji rofi-power-menu ];
         font = "JetBrains Mono Regular 13";
     };
 }
